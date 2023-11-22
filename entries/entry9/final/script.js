1b441b0bf6dd3c885a33a757d08766fa456a9789
singd123 <singd123@newschool.edu>
2023-11-21 19:13:03 -0500

    hi

--- entries/entry9/final/script.js
+++ entries/entry9/final/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
     
-    // Simulate a simple sentence generator
+   
     var feelings = ["Excited", "Curious", "Nervous", "Reflective", "Empowered"];
     var generatorIndex = 0;
 
@@ -10,7 +10,7 @@ $(document).ready(function () {
         generatorIndex = (generatorIndex + 1) % feelings.length;
     });
 
-    // Handle color picker change event
+ 
     $('#colorPicker').change(function () {
         var selectedColor = $(this).val();
         $('body').css('background-color', selectedColor);
